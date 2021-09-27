375c3c5bcd48c1a9ee9e08a075aee02dbca84552
Kevin-carrasco <kevin.carrasco@ug.uchile.cl>
2021-09-27 10:35:22 -0300

    correcciones cap 1 y desarrollo cap 2
    Se incluyen correcciones de orden y redaccion en cap 1
    se incluye desarrollo mas detallado de graficos en cap 2

--- bookdown-files/trackdown.R
+++ bookdown-files/trackdown.R
@@ -1,15 +1,14 @@
 library(trackdown)
 
+# Sube el archivo
 upload_file(file = "02-cambios.Rmd",
             gpath = "cap2-cohesion",
             hide_code = TRUE,
 )
 
-### Update_file actualiza el documento en el drive. no el rmarkdown
-update_file(file= "prueba.Rmd",
-            gpath = "prueba")
 
-trackdown::download_file(file = "prueba.Rmd",
-                         gpath = "prueba")
+# Actualiza el rmd con los cambios en el drive
+trackdown::download_file(file = "02-cambios.Rmd",
+                         gpath = "cap2-cohesion")
 
 ## Ojo, problemas. No se copian los tildes, negritas, cursivas.
